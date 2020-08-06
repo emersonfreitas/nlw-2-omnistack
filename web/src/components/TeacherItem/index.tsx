89e861888bba3791e77d72d2cf11c16f48650049
Emerson Freitas <emersonfreitasn@gmail.com>
2020-08-06 10:56:59 -0500

    ✨ web: configuring communication with the api, adding a new class form and class list

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,35 +3,53 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: string;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://pbs.twimg.com/profile_images/1236049480367169543/ISGkKiHL_400x400.jpg" alt="Emerson Freitas" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Emerson Freitas</strong>
-          <span>Inglês</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing.
-
-        <br /> <br />
-
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil suscipit officia quas reprehenderit nisi dolorem laudantium, excepturi corrupti nam natus dolorum accusantium velit. Ducimus voluptatum, vero similique provident voluptas officiis.
+        {teacher.bio}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$30,00</strong>
+          <strong>R${teacher.cost}</strong>
         </p>
-        <button type="button">
+        // eslint-disable-next-line react/jsx-no-target-blank
+        <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}?text=${'Olá'}`} target="_blank" >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
